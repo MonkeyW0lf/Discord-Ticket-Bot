9553d78322662efdd2de8430d688d32d6a9396e6
MonkeyWolf <107629038+MonkeyW0lf@users.noreply.github.com>
2023-05-04 19:51:44 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,10 +2,12 @@ token = "TOKEN"
 
 logo = "LIEN DIRECTE VERS VOTRE LOGO"
 
+# texte en dessous des embed
 funfact = "J'ai été conçu par MonkeyWolf"
 
+# le status du bot (ex: Joue à {ton texte})
 status = "J'ai été conçu par MonkeyWolf"
-
+# le nom exacte des catégories
 categorie_partenariat = "Partenariat"
 categorie_repport = "REPORT"
 categorie_commande = "COMMANDE"
